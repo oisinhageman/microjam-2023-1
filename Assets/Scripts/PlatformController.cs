cf69105618ee6828402423dee792a55bb290cd52
Oisín Hageman <oisin@haagfens.nl>
2023-02-05 16:40:22 +0100

    add platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class PlatformController : MonoBehaviour
 {
+    public float width = 4.0f;
+    public Rigidbody2D rb;
+    public BoxCollider2D bc;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        bc.size = new Vector2(width, bc.size.y);
     }
 
     // Update is called once per frame
